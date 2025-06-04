33aa1584d9bbfc81723ed1a54a54df4cdc6ce210
Himanshu Verma <himanshhhhuv@gmail.com>
2025-06-04 11:32:12 +0530

    hjvgghc

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,18 +8,15 @@ const Navbar = async () => {
     <div className="flex items-center justify-between p-4">
       {/* SEARCH BAR */}
       <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
-        <Image src="/search.png" alt="" width={14} height={14} />
-        <input
-          type="text"
-          placeholder="Search..."
-          className="w-[200px] p-2 bg-transparent outline-none"
-        />
+        <span className="text-lg text-gray-800 text-right">
+          {user?.publicMetadata?.roll as string}
+        </span>
       </div>
       {/* ICONS AND USER */}
       <div className="flex items-center gap-6 justify-end w-full">
-        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+        {/* <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
           <Image src="/message.png" alt="" width={20} height={20} />
-        </div>
+        </div> */}
         {/* <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
           <Image src="/announcement.png" alt="" width={20} height={20} />
           <div className="absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs">
@@ -27,8 +24,10 @@ const Navbar = async () => {
           </div>
         </div> */}
         <div className="flex flex-col">
-          <span className="text-xs leading-3 font-medium">User</span>
-          <span className="text-[10px] text-gray-500 text-right">
+          <span className="text-md leading-3 font-medium">
+            {user?.publicMetadata?.name as string}
+          </span>
+          <span className="text-xs text-gray-500 text-right">
             {user?.publicMetadata?.role as string}
           </span>
         </div>
